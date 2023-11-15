a9508ba91ee2fca2f0bbfb909f3ccb91870fff4a
tim2004timi <tim2004timi@mail.ru>
2023-11-15 17:35:12 +0300

    Add tests

--- posts/models.py
+++ posts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Post(models.Model):
     text = models.TextField()
+    objects = models.Manager()
 
     def __str__(self):
         return self.text[:20]
-    
